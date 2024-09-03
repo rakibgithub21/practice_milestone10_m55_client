7b291641d403655ccc4c26a7d7eecb4a50882260
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-09-03 16:25:50 +0600

    make get read delete api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(data => setUsers(data))
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+
+  console.log(users, 'all users');
+
+
+  const handleDeleteUser = (id) => {
+    console.log(id);
+    fetch(`http://localhost:5000/users/${id}`, {
+      method:'DELETE'
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.deletedCount > 0) {
+          alert('deleted succesfully')
+          const remaining = users.filter(user => user._id !== id)
+          setUsers(remaining)
+        }
+      })
+  }
+
 
   const handleAddUser = (e) => {
     e.preventDefault()
@@ -18,7 +54,7 @@ function App() {
       headers: {
         'content-type': 'application/json'
       },
-      body:JSON.stringify(user)
+      body: JSON.stringify(user)
     })
       .then(res => res.json())
       .then(data => {
@@ -44,6 +80,12 @@ function App() {
         <br />
         <input type='submit' value="Add User" className="btn btn-active btn-ghost"></input>
       </form>
+
+      <div>
+        {
+          users.map(user => <div key={user._id}>{user.name} <button onClick={() => handleDeleteUser(user._id)} className='btn btn-secondary'>x</button></div>)
+        }
+      </div>
     </div>
   )
 }
